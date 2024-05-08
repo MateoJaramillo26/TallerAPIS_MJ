65dc4c72ab1b7db835d8d009da0aeb4a9eaa1c86
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-05-08 11:44:54 -0500

    Add project files.

---
+++ Data/TallerMVC2_MJContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TallerMVC3_MJ.Models;
+
+namespace TallerMVC3_MJ.Data
+{
+    public class TallerMVC2_MJContext : DbContext
+    {
+        public TallerMVC2_MJContext (DbContextOptions<TallerMVC2_MJContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TallerMVC3_MJ.Models.Burger> Burger { get; set; } = default!;
+        public DbSet<TallerMVC3_MJ.Models.Promo> Promo { get; set; } = default!;
+    }
+}
---
+++ Models/Burger.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerMVC3_MJ.Models
+{
+    public class Burger
+    {
+        public int BurgerId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [VerificarRango]
+        public decimal Precio { get; set; }
+
+        public List<Promo>? Promo { get; set; }
+    }
+
+    public class VerificarRango : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            decimal valor = (decimal)value;
+            if (valor < 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
